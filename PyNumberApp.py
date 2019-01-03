69b7ea8300878c965b8b42cb711ff5099c6f877b
Gunner <gdhatmaker@gmail.com>
2019-01-03 16:01:39 +0700

    Update PyNumberApp.py

--- PyNumberApp.py
+++ PyNumberApp.py
@@ -1,85 +1,82 @@
 import os
 import re
+import operator
 #path = './numbers.txt'
+sums = {}
 highest = {}
 lowest = {}
+averages = {}
 
-def get_file(file_name):
-    path = file_name
-    number_file = open(path, 'r')  
-    number_read = number_file.readlines()
-    linenumber = 1
-    for line in number_read:
-        line = line.strip("\n")
-        line = line.strip('')
-        line = line.replace("\n","")
-        line = line.split(" ")
-        line = filter(bool, line)
-        line = list(map(int, line))
-        check_size(line)
-        print("line number: " + str(linenumber))
-        check_text(line)
-        add_numbers(line)
-        average_numbers(line)
-        get_highest(line, linenumber)
-        get_lowest(line, linenumber)
-        print(highest)
-        print(line)
-        linenumber+=1
-    return_highest(highest)
-    return_lowest(lowest)
-    number_file.close()
-    
-def add_numbers(file):
-    count = int(0)
-    for n in file:
-        if n == "":
-            pass
-        else:
-            #line = re.sub(r"[\n\t\s]*", "", line)
-            count = int(n) + int(count)
-    print( " Toal is: " + str(count))
-    return count
-def average_numbers(line):
-    amount = 0
-    count = 0
-    for n in line:
-        amount = amount + 1
-        if n == "":
-            pass
-        else:
-            count = int(n) + int(count)
-    average = int(count)/int(amount)
-    print("Average is " + str(round(average)))
-    return round(average)
+class NumberCheck():
+    def __init__(self):
+        self = self
+    def get_file(file_name):
+        path = file_name
+        number_file = open(path, 'r')  
+        number_read = number_file.readlines()
+        lines = []
+        for line in number_read:
+            line = line.strip("\n")
+            line = line.strip('')
+            line = line.split(" ")
+            line = filter(bool, line)
+            line = list(map(int, line))
+            lines.append(line)
+        return lines
+        number_file.close()
+class AddNumber():
+    def __init__(self):
+        self = self
+    def add_numbers():
+        lineNumber = 1
+        for eachNumber in numberlist:
+            sums[lineNumber] = sum(eachNumber)
+            lineNumber+=1
+        print("Sums are :" + str(sums))
+        return sums
+class AverageNumbers():
+    def __init__(self):
+        self = self
+    def average_numbers():
+        lineNumber = 1
+        for eachNumber in numberlist:
+            total = sum(eachNumber)
+            average = round(total/(len(eachNumber) + 1))
+            averages[lineNumber] = average
+            lineNumber+=1
+        print("averages are " + str(averages))
+        return averages
 
-def get_highest(line, line_num):
-        highest[line_num]= max(line)
-        return max(line)
-
-def return_highest(list):
-    maximum = max(list, key=list.get)
-    print("highest is Line " + str(maximum) + " : " + str(list[maximum]))
-    return list[maximum]
-
-def get_lowest(line, line_num):
-    lowest[line_num] = min(line)
-    return min(line)
-
-def return_lowest(list):
-    minimum = min(list, key=list.get)
-    print("Lowest is line " + str(minimum) + " : " + str(list[minimum]))
-    return list[minimum]
-def check_size(list):
-    for n in list:
-        if int(n) > 99999999:
-            raise Exception("Number too big")
-    else:
-        return
-def check_text(list): ### doesn't matter. map to int will throw error.
-    for n in list:
-        if type(n) is str:
-            raise Exception("Text inside document")
+class HighestNumber():
+    def __init__(self):
+        self = self
+    def highest_number():
+        current = 0 
+        lineNumber = 1
+        maxnumbers = {}
+        for eachNumber in numberlist:
+            maxnumbers[lineNumber] = max(eachNumber)
+            lineNumber+=1
+        highest = max(zip(maxnumbers.keys(), maxnumbers.values()))
+        print("Highest number is on line: " + str(highest[0]) + " and the highest number is " + str(highest[1]))
+        return highest
+class LowestNumber():
+    def __init__(self):
+        self = self
+    def lowest_number():
+        current = 0 
+        lineNumber = 1
+        minnumbers = {}
+        for eachNumber in numberlist:
+            minnumbers[lineNumber] = min(eachNumber)
+            lineNumber+=1
+        lowest = min(zip(minnumbers.keys(), minnumbers.values()))
+        print("Lowest number is on line: " + str(lowest[0]) + " and the lowest number is " + str(lowest[1]))
+        return lowest
+numberlist = NumberCheck.get_file('./numbers.txt') 
+AddNumber.add_numbers()
+AverageNumbers.average_numbers()
+HighestNumber.highest_number()
+LowestNumber.lowest_number()
 
 
-get_file('./numbers.txt')
